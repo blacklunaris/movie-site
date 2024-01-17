00476b94f97868fe2c236e77863dce4e58b87dcd
Michael Pearson <mpearson@realdecoy.com>
2024-01-16 23:36:07 -0500

    Added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MovieCard from './components/MovieCard';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>
+  },
+  {
+    path: "/movies",
+    element: <MovieCard MovieName={"Test1"} Season={"Test2"} Episode={"Test3"} EpisodeTitle={"Test4"} />
+  }
+]);
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement ?? document.createElement("div"));
+root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
